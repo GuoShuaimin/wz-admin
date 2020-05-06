43e7dc1f49cfd214454eb91db887a1548fe0a313
GuoShuaimin <996595179@qq.com>
2020-05-06 15:30:47 +0800

    first commit

---
+++ src/components/common/header.vue
@@ -0,0 +1,15 @@
+<template>
+	<div class="header">
+		<div class="collapse-btn" @click="collapseChage">
+			<i v-if="!collapse" class="el-icon-s-fold"></i>
+			<i v-else class="el-icon-s-unfold"></i>
+		</div>
+		<div class=""></div>
+	</div>
+</template>
+
+<script>
+</script>
+
+<style>
+</style>
